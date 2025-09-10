26782fe1b62d8d92518c58c8f174ae5e1f46c3bb
arkit_k <arkitkarmokar007@gmail.com>
2025-09-10 14:59:46 +0530

    commited few logs & resolved error

--- app/chat/eternals/page.tsx
+++ app/chat/eternals/page.tsx
@@ -254,7 +254,7 @@ export default function ChatwithKYemon() {
                 }}
                 className="absolute inset-0 w-full h-full object-cover"
               >
-                <source src="/backgrounds/eternalbg.mp4" type="video/mp4" />
+                <source src="/backgrounds/eternalbg2.mp4" type="video/mp4" />
               </video>
             )}
           </>
